1d57d5abb9a9082a8235d2fb105f497a5974584b
Allan Juma <allancjuma@gmail.com>
2023-08-29 16:29:34 +0300

    notes update

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
