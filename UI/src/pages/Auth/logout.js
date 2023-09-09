f511f5051379d7a80c97a44324d281639230527d
Allan Juma <allancjuma@gmail.com>
2023-09-09 18:19:57 +0300

    finalized client logout

--- UI/src/pages/Auth/logout.js
+++ UI/src/pages/Auth/logout.js
@@ -1,10 +1,29 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { userLogout } from "redux/features/user/userSlice";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsLoggedIn, userLogout } from "redux/features/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
   const dispatch = useDispatch();
-  dispatch(userLogout());
+  const navigate = useNavigate();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  useEffect(() => {
+    const logoutAsync = () => {
+      try {
+        isLoggedIn && dispatch(userLogout());
+
+        // If the logout was successful, navigate to the login page
+        !isLoggedIn && navigate("/login");
+      } catch (error) {
+        // Handle any errors if needed
+        console.error("Logout failed: ", error);
+      }
+    };
+
+    logoutAsync();
+  }, [dispatch, navigate, isLoggedIn]);
+
   return <div>Logout</div>;
 };
 
