a4cae0f9884031b098dfdb8d10bc60b7bc3ad35d
Allan Juma <allancjuma@gmail.com>
2023-09-11 15:40:52 +0300

    ignore redux persist actions

--- UI/src/redux/store.js
+++ UI/src/redux/store.js
@@ -1,6 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import rootReducer from "./features";
 
 const persistConfig = {
@@ -12,6 +21,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
