bcc2b463c2a0b62066fb8e9bbb6c22f0d9cb6825
Allan Juma <allancjuma@gmail.com>
2023-09-11 16:44:59 +0300

    error handler update

--- Backend/middleware/errorHandler.js
+++ Backend/middleware/errorHandler.js
@@ -8,7 +8,7 @@ const errorHandler = (error, req, res, next) => {
       logger.error(error.message, { metadata: error, stack: error.stack });
       return res.status(400).json({ message: error.message });
     default:
-      logger.error(error.message);
+      logger.error(error.message, { metadata: error, stack: error.stack });
       return res.status(500).json({ message: Messages.serverError });
   }
 };
