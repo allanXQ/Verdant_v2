caa1e732c9edefbc27d747ce3b5d28e80a8ce28f
Allan Juma <allancjuma@gmail.com>
2023-09-21 14:47:40 +0300

    cleanup

--- UI/src/components/Navigation/Navbar.js
+++ UI/src/components/Navigation/Navbar.js
@@ -205,8 +205,6 @@ function ResponsiveDrawer(props) {
           </div>
         ))}
       </List>
-
-      <Divider />
     </Box>
   );
 
@@ -243,14 +241,13 @@ function ResponsiveDrawer(props) {
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
         aria-label="mailbox folders"
       >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Drawer
           container={container}
           variant="temporary"
           open={mobileOpen}
           onClose={handleDrawerToggle}
           ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
+            keepMounted: true,
           }}
           sx={{
             display: { xs: "block", sm: "none" },
