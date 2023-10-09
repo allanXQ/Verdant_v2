8951e688f1eb28fe77384fd9d9a918ff6aeebf76
Allan Juma <allancjuma@gmail.com>
2023-10-09 11:43:18 +0300

    testing stepper implemetation

--- UI/src/components/forms/models/deposit.js
+++ UI/src/components/forms/models/deposit.js
@@ -1,30 +1,47 @@
 import CreateForm from "../utils/createForm";
+import FormStepper from "../utils/formStepper";
 
 const DepositModel = {
   name: "Deposit",
   endpoint: "user/transact/mpesa/deposit",
   method: "post",
+  isStepper: true,
 
-  fields: [
+  steps: [
     {
-      name: "phone",
-      type: "number",
-      label: "Mpesa number",
-      placeholder: "2547...",
-      required: true,
+      title: "Step 1",
+      fields: [
+        {
+          name: "phone",
+          type: "number",
+          label: "Mpesa number",
+          placeholder: "2547...",
+          required: true,
+        },
+        {
+          name: "amount",
+          type: "number",
+          label: "Amount",
+          placeholder: "min 100",
+          required: true,
+        },
+      ],
     },
     {
-      name: "amount",
-      type: "number",
-      label: "Amount",
-      placeholder: "min 100",
-      required: true,
+      title: "Confirm form details",
+      confirmation: true,
+      fields: [],
+    },
+    {
+      title: "Submit form",
+      submission: true,
+      fields: [],
     },
   ],
 };
 
 const DepositForm = () => {
-  return CreateForm("Deposit", DepositModel);
+  return FormStepper("Deposit", DepositModel);
 };
 
 export default DepositForm;
