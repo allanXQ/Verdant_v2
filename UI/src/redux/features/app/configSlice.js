82e55e23fc0994c254e6f9b62e4545ec9ee5da03
Allan Juma <allancjuma@gmail.com>
2023-10-09 19:20:53 +0300

    removed unnecessary state

--- UI/src/redux/features/app/configSlice.js
+++ UI/src/redux/features/app/configSlice.js
@@ -2,12 +2,6 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   theme: "light",
-
-  sideBar: {
-    width: "210px",
-    isOpen: false,
-    isCollapsed: false,
-  },
   topBar: {
     height: "50px",
   },
@@ -39,10 +33,6 @@ export const configSlice = createSlice({
 
 export const { updateTheme, updateMessageModal } = configSlice.actions;
 export const selectTheme = (state) => state.config.theme;
-export const selectSideBarWidth = (state) => state.config.sideBar.width;
-export const selectSideBarIsOpen = (state) => state.config.sideBar.isOpen;
-export const selectSideBarIsCollapsed = (state) =>
-  state.config.sideBar.isCollapsed;
 export const selectTopBarHeight = (state) => state.config.topBar.height;
 export const selectDrawerHeight = (state) => state.config.drawer.height;
 export const selectDrawerWidth = (state) => state.config.drawer.width;
