d63c724c44c17672e1596db012ce64f498a32e10
Allan Juma <allancjuma@gmail.com>
2023-10-11 04:28:20 +0300

    fixed entire controller

--- Backend/controllers/app/Assetinfo/getTickerData.js
+++ Backend/controllers/app/Assetinfo/getTickerData.js
@@ -1,34 +1,33 @@
 const { findQuery } = require("@services");
 const Messages = require("@utils/messages");
-const { klineIntervals } = require("./index");
+const { klineIntervals, coinLabelMap } = require("./index");
 const { Assets } = require("@models");
 const { default: axios } = require("axios");
 
 const getTickerData = async (req, res, next) => {
-  const assets = await findQuery(Assets);
-  const assetUpdate = assets.map(async (asset) => {
-    const tradingPair = asset.coinPair;
-    const url = `https://api.binance.com/api/v3/ticker/24hr?symbol=${tradingPair}`;
-    try {
-      const response = await axios.get(url);
-      const data = response.data;
-      asset.price = parseFloat(data.lastPrice, 10);
-      asset.priceChange = parseFloat(data.priceChange, 10);
-      return asset;
-    } catch (err) {
-      console.log(err.message);
-    }
-  });
-
-  await Promise.all(assetUpdate);
-
-  return res.status(200).json({
-    message: Messages.requestSuccessful,
-    payload: {
-      assets,
-      klineIntervals,
-    },
-  });
+  const { assetName } = req.body;
+  const tradingPair = coinLabelMap[assetName];
+  if (!tradingPair) {
+    throw new Error(Messages.invalidAsset);
+  }
+  const url = `https://api.binance.com/api/v3/ticker/24hr?symbol=${tradingPair}`;
+  let tickerData = {};
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    tickerData.high = parseFloat(data.highPrice).toFixed(2);
+    tickerData.low = parseFloat(data.lowPrice).toFixed(2);
+    tickerData.volume = parseFloat(data.volume).toFixed(2);
+    tickerData.priceChange = parseFloat(data.priceChange).toFixed(2);
+    return res.status(200).json({
+      message: Messages.requestSuccessful,
+      payload: {
+        ...tickerData,
+      },
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
 module.exports = getTickerData;
