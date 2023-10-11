ec1829c86ae41e9fda2b12495228d717ad877397
Allan Juma <allancjuma@gmail.com>
2023-10-12 02:53:11 +0300

    added buy order controller

--- Backend/controllers/user/Portfolio/buyLimit.js
+++ Backend/controllers/user/Portfolio/buyLimit.js
@@ -1,12 +1,13 @@
 const { default: mongoose } = require("mongoose");
 const Messages = require("@utils/messages");
-const closedTrades = require("@models/p2p/closedPeer");
-const Users = require("@models/users");
-const sellOrders = require("@models/p2p/peerOrders");
-const Escrow = require("@models/p2p/peerEscrow");
+const { User, limitOrders, limitEscrow } = require("@models");
+const axios = require("axios");
+const { coinLabelMap } = require("@config");
 
-//check if sell order exists
 //check if buyer has sufficient account balance
+//check if sell order exists
+//if !exists create limit buy order, update buyer account balance, create escrow account
+
 //check if seller escrow account exists with this orderid and has sufficient asset balance
 //update buyer account balance -
 //update seller account balance +
@@ -14,9 +15,25 @@ const Escrow = require("@models/p2p/peerEscrow");
 //update buyer portfolio balance +
 //remove/update order from sellOrders
 //create a complete trade
+const fetchTickerData = async (assetName) => {
+  const tradingPair = coinLabelMap[assetName];
+  if (!tradingPair) {
+    throw new Error(Messages.invalidAsset);
+  }
+  const url = `https://api.binance.com/api/v3/ticker/24hr?symbol=${tradingPair}`;
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    let lastPrice = parseFloat(data.lastPrice).toFixed(2);
+    return parseInt(lastPrice);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
 
 const buyLimit = async (req, res) => {
-  const { userId, sellerId, orderId, stockName, stockAmount, price } = req.body;
+  const { userId, sellerId, assetName, amount } = req.body;
+  const price = await fetchTickerData(assetName);
   if (userId === sellerId) {
     return res.status(400).json({ message: Messages.invalidRequest });
   }
@@ -25,111 +42,52 @@ const buyLimit = async (req, res) => {
     session = await mongoose.startSession();
     session.startTransaction();
 
-    //check order existence
-    const Order = await sellOrders.findOne({ orderId, price });
-    if (!Order) {
-      return res.status(400).json({ message: Messages.invalidRequest });
-    }
-    if (parseInt(Order.stockAmount) < parseInt(stockAmount)) {
-      return res.status(400).json({ message: Messages.overRequest });
-    }
+    //find buyer and order
+    const Buyer = await User.findOne({ userId }).session(session);
+    const Order = await limitOrders
+      .findOne({ assetName, price })
+      .session(session);
 
-    //check if buyer has sufficient account balance
-    const buyer = await Users.findOne({ userId }).session(session);
-    const buyerAccountBalance = parseInt(buyer.accountBalance);
-    const assetValue = parseInt(stockAmount) * parseInt(price);
-    if (buyerAccountBalance < assetValue) {
+    const buyerBalance = parseInt(Buyer.balance);
+    const assetAmount = parseInt(amount);
+    const totalCost = assetAmount * price;
+    if (buyerBalance < totalCost) {
       return res.status(400).json({ message: Messages.insufficientBalance });
     }
-    //check if seller escrow account exists with this orderid and has sufficient asset balance
-    const sellerEscrow = await Escrow.findOne({ orderId, userId: sellerId });
-    if (
-      !sellerEscrow ||
-      parseInt(sellerEscrow.stockAmount) < parseInt(stockAmount)
-    ) {
-      return res.status(400).json({ message: Messages.invalidRequest });
-    }
-    //update buyer account balance -
-    buyer.accountBalance = buyerAccountBalance - assetValue;
-    await buyer.save({ session });
-
-    //update seller account balance +
-    const seller = await Users.findOne({ userId: sellerId }).session(session);
-    if (!seller) {
-      return res.status(400).json({ message: Messages.invalidRequest });
-    }
-    const sellerAccountBalance = parseInt(seller.accountBalance);
-    seller.accountBalance = sellerAccountBalance + assetValue;
-    await seller.save({ session });
-
-    //update/delete seller escrow account -
-    const sellerStockAmount = parseInt(sellerEscrow.stockAmount);
-    const newSellerEscrowBalance = sellerStockAmount - parseInt(stockAmount);
-    if (newSellerEscrowBalance < 0) {
-      return res.json({
-        status: 400,
-        message: Messages.invalidRequest,
+    if (Order) {
+      const sellerId = Order.userId;
+      const orderId = Order.orderId;
+      const Seller = await User.findOne({ userId: sellerId }).session(session);
+      const orderEscrow = await limitEscrow
+        .findOne({ orderId })
+        .session(session);
+      const sellerBalance = parseInt(Seller.balance);
+      const escrowAmount = parseInt(orderEscrow.amount);
+      Buyer.balance = buyerBalance - totalCost;
+      Seller.balance = sellerBalance + totalCost;
+      const newEscrowAmount = escrowAmount - assetAmount;
+      orderEscrow.amount = newEscrowAmount;
+      if (newEscrowAmount <= 0) {
+        await limitEscrow.deleteOne({ orderId }).session(session);
+      }
+      const buyerPortfolio = Buyer.portfolio;
+      buyerPortfolio.find((asset) => {
+        if (asset.assetName === assetName) {
+          asset.amount = parseInt(asset.amount) + assetAmount;
+        }
       });
+      await Buyer.save();
+      await Seller.save();
+      await orderEscrow.save();
+      await session.commitTransaction();
+      session.endSession();
+      return res.status(200).json({ message: Messages.orderCompleted });
     }
-    if (newSellerEscrowBalance === 0) {
-      await Escrow.deleteOne({ orderId, userId: sellerId }).session(session);
-    } else {
-      await Escrow.updateOne(
-        { orderId, userId: sellerId },
-        {
-          $set: {
-            stockAmount: newSellerEscrowBalance,
-          },
-        },
-        { session }
-      );
-    }
-
-    //update/delete order from sell orders
-
-    if (parseInt(Order.stockAmount) - parseInt(stockAmount) === 0) {
-      //delete order
-      await sellOrders.deleteOne({ orderId }).session(session);
-    } else {
-      //update order
-      await sellOrders.updateOne(
-        { orderId },
-        {
-          $set: {
-            stockAmount: newSellerEscrowBalance,
-          },
-        },
-        { session }
-      );
-    }
-
-    //create closed trade
-
-    const sellLimit = await closedTrades.create(
-      [
-        {
-          orderId,
-          buyerId: userId,
-          sellerId,
-          stockName,
-          stockAmount,
-          price,
-        },
-      ],
-      { session }
-    );
-    await session.commitTransaction();
-    session && session.endSession();
-
-    return res.json({
-      status: 200,
-      payload: sellLimit,
-      message: Messages.orderCreated,
-    });
-  } catch (error) {
-    session && (await session.abortTransaction());
-    session && session.endSession();
-    throw error;
+  } catch (err) {
+    console.log(err);
+    await session.abortTransaction();
+    session.endSession();
+    return res.status(500).json({ message: err.message });
   }
 };
 
