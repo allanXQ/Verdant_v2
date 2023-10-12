8ad2b9eb8b0b0936a5e5927415f5756d53079b19
Allan Juma <allancjuma@gmail.com>
2023-10-13 02:37:42 +0300

    proper async handling

--- Backend/controllers/app/Assetinfo/getAssets.js
+++ Backend/controllers/app/Assetinfo/getAssets.js
@@ -1,20 +1,22 @@
 const Messages = require("@utils");
 const { Assets } = require("@models");
+const fetchTickerData = require("@utils/fetchTicker");
 
 const getAssets = async (req, res) => {
-  const assets = await Assets.find();
+  const assets = await Assets.find().lean();
 
-  for (let i = 0; i < assets.length; i++) {
-    const asset = assets[i];
+  const promises = assets.map(async (asset) => {
     const { assetName } = asset;
     const { lastPrice, priceChange } = await fetchTickerData(assetName);
     asset.lastPrice = lastPrice;
     asset.priceChange = priceChange;
-  }
+    return asset;
+  });
+  const newAssets = await Promise.all(promises);
   return res.status(200).json({
     message: Messages.requestSuccessful,
     payload: {
-      assets,
+      newAssets,
     },
   });
 };
